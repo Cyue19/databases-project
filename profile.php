55d99046c04e76630b586f1db38da1ff78a59e77
Cyue19 <yue_cheryl@hotmail.com>
2022-03-29 23:46:48 -0400

    outlined basic setup and navbar

---
+++ profile.php
@@ -0,0 +1,45 @@
+<!-- 1. create HTML5 doctype -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">  
+  
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">  
+    
+  <title>My Profile</title>
+  
+  <!-- 3. link bootstrap -->
+  <!-- if you choose to use CDN for CSS bootstrap -->  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  
+  <!-- If you choose to use a favicon, specify the destination of the resource in href -->
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  
+  <!-- include your CSS -->
+  <!-- <link rel="stylesheet" href="custom.css" />  -->
+       
+</head>
+
+<body>
+    <?php
+        include("navBar.php");
+    ?>
+
+    <div class="container">
+    <h1>My Profile</h1>  
+
+    <!-- CDN for JS bootstrap -->
+    <!-- you may also use JS bootstrap to make the page dynamic -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+    <!-- for local -->
+    <!-- <script src="your-js-file.js"></script> -->  
+    
+    </div>    
+</body>
+</html>
