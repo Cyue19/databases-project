b948b31f91bbc1d53cceb6ccdd4b594133992ea0
arxk9 <az4xfp@virginia.edu>
2022-04-17 18:31:57 -0400

    hotfix

--- upload.php
+++ upload.php
@@ -21,21 +21,23 @@ if(isset($_POST["submit"])) {
     global $db;
     $file = fopen($filename, "r");
     while (($getData = fgetcsv($file, 10000, ",")) !== FALSE) {
-        $sql = "INSERT into Media (emp_id,firstname,lastname,email,reg_date) 
-             values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."')";
-        $result = mysqli_query($db, $sql);
-        if(!isset($result)) {
-          echo "<script type=\"text/javascript\">
-              alert(\"Invalid File:Please Upload CSV File.\");
-              window.location = \"index.php\"
-              </script>";    
-        }
-        else {
-            echo "<script type=\"text/javascript\">
-            alert(\"CSV File has been successfully Imported.\");
+      // put sql commands and stuff here
+
+      $sql = "INSERT into Media (emp_id,firstname,lastname,email,reg_date) 
+           values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."')";
+      $result = mysqli_query($db, $sql);
+      if(!isset($result)) {
+        echo "<script type=\"text/javascript\">
+            alert(\"Invalid File:Please Upload CSV File.\");
             window.location = \"index.php\"
-          </script>";
-        }
+            </script>";    
+      }
+      else {
+          echo "<script type=\"text/javascript\">
+          alert(\"CSV File has been successfully Imported.\");
+          window.location = \"index.php\"
+        </script>";
+      }
     }
     fclose($file);  
   }
