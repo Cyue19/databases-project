99614a053cc74d7e3638da141e3ed04d7f862835
jennifersheng <jensheng24@gmail.com>
2022-04-17 10:47:12 -0400

    replacing with correct navbar code

--- navBar.php
+++ navBar.php
@@ -4,103 +4,56 @@
 <head>
   <meta charset="UTF-8">  
   
-  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
-  <meta name="author" content="your name">
-  <meta name="description" content="include some description about your page">  
-    
-  <title>My Profile</title>
-  
-  <!-- 3. link bootstrap -->
-  <!-- if you choose to use CDN for CSS bootstrap -->  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
   
   <!-- If you choose to use a favicon, specify the destination of the resource in href -->
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  
   
   <!-- include your CSS -->
   <!-- <link rel="stylesheet" href="custom.css" />  -->
        
 </head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #440E79">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">SSDB</a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" aria-current="page" href="index.php">browse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="watch_again.php">watch again</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="cont_watch.php">continue watching</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="wishlist.php">wishlist</a>
+                    </li>
+                </ul>
+                
+                <form>
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                </form>
+                
+                <li>
+                    <a href="profile.php">
+                        <i class="fas fa-user-circle fa-2x" style="color: white;"></i>
+                    </a>
+                </li>
+            </div>
+        </div>
+    </nav>
 
-<body style="background-color: #0F1C48">
-    <?php
-        include("navBar.php");
-    ?>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
-
-<div class="container">
-<div class="row justify-content-md-center">
-	<div class="col-10 text-white" style="background-color: #091436">
-		<!-- Page title -->
-		<div class="my-5">
-			<h3>Edit Account</h3>
-			<hr>
-		</div>
-		<!-- Form START -->
-		<form class="file-upload">
-			<div class="row mb-5 gx-5">
-				<!-- account detail -->
-				<div class="col-xxl-8 mb-5 mb-xxl-0">
-					<div class="bg-secondary-soft px-4 py-5 rounded">
-						<div class="row g-3">
-							<h4 class="mb-4 mt-0">Account Details</h4>
-							<!-- First Name -->
-							<div class="col-md-6">
-								<label class="form-label">First Name:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="First name" value="John">
-							</div>
-							<!-- Last name -->
-							<div class="col-md-6">
-								<label class="form-label">Last Name:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="Last name" value="Smith">
-							</div>
-							<!-- Phone number -->
-							<div class="col-md-6">
-								<label class="form-label">Email:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="Phone number" value="example@gmail.com">
-							</div>
-							<!-- Mobile number -->
-							<div class="col-md-6">
-								<label class="form-label">Password:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="Phone number" value="********">
-							</div>
-						</div> <!-- Row END -->
-					</div>
-				</div>					
-		<!-- personal preferences -->
-				<div class="col-xxl-8 mb-5 mb-xxl-0">
-					<div class="bg-secondary-soft px-4 py-5 rounded">
-						<div class="row g-3">
-							<h4 class="mb-4 mt-0">Personal Preferences</h4>
-							<!-- First Name -->
-							<div class="col-md-12">
-								<label class="form-label">Favorite Genre:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="First name" value="Comedy">
-							</div>
-							<!-- Last name -->
-							<div class="col-md-12">
-								<label class="form-label">Favorite Movie:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="Last name" value="Star Wars">
-							</div>
-							<!-- Phone number -->
-							<div class="col-md-12">
-								<label class="form-label">Favorite Show:</label>
-								<input type="text" class="form-control" placeholder="" aria-label="Phone number" value="The Office">
-							</div>
-						</div> <!-- Row END -->
-					</div>
-				</div>
-			<!-- button -->
-			<div class="col text-center">
-				<button type="button" class="btn btn-primary">Update profile</button>
-			</div>
-		</form> <!-- Form END -->
-		</div>
-	</div>
-	</div>
+  <!-- CDN for JS bootstrap -->
+  <!-- you may also use JS bootstrap to make the page dynamic -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  
+  <!-- for local -->
+  <!-- <script src="your-js-file.js"></script> -->    
 </body>
 </html>
