50729433e66d51dc1a530220fad5120072b1584e
jennifersheng <jensheng24@gmail.com>
2022-04-17 23:58:26 -0400

    fixed background

--- media_page.php
+++ media_page.php
@@ -156,6 +156,7 @@
 
 ?>
 
+<!-- 1. create HTML5 doctype -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -163,16 +164,17 @@
   
   <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  
   <meta name="author" content="your name">
   <meta name="description" content="include some description about your page">  
-
-  <title>Media Description</title>
+    
+  <title>My Profile</title>
   
   <!-- 3. link bootstrap -->
   <!-- if you choose to use CDN for CSS bootstrap -->  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  
   <!-- If you choose to use a favicon, specify the destination of the resource in href -->
   <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
   
@@ -180,11 +182,11 @@
   <!-- <link rel="stylesheet" href="custom.css" />  -->
        
 </head>
-<body style="background-color: white">
-  <?php
-    // session_start();
-    include("navBar.php");
-  ?>
+
+<body style="background-color: #0F1C48">
+    <?php
+        include("navBar.php");
+    ?>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
 
